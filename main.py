cf9ca7e935da7aaa4e4d62d1d229563e2b27a899
干净T松鼠 <81066358+Andy17269@users.noreply.github.com>
2023-11-25 19:40:28 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import os
-
 import requests
 import time
-
+import warnings
+warnings.filterwarnings("ignore")
 amap_key = '1e4fce5cc58bc22229fe6cf0814074f1'
 qw_key = '781742beafdc4ddc8e0d7a3e46fd113c'
-
 try:
     config_info_get = requests.get('https://assets.wenlei.club/wenweather/config.json')
     config_info = config_info_get.json()
@@ -20,8 +18,6 @@ except:
     print('无法连接网络，或服务器正在维护')
 
 print('欢迎使用 {} For Terminal\n'.format(wen_name))
-
-time.sleep(0.6)
 
 try:
     ageo_get = requests.get('https://restapi.amap.com/v3/ip?key={}'.format(amap_key))
@@ -39,15 +35,13 @@ try:
     reporttime = wa.get('lives')[0].get("reporttime")
 except:
     print('无法连接至天气数据服务器')
-
-print('功能列表：\n1.查询所在地天气\n2.查询其它城市天气\n3.退出 {}\n4.关于 {}\n'.format(wen_name, wen_name))
+print('选择功能：\n1.查询所在地天气\n2.查询其它城市天气\n3.退出 {}\n4.关于 {}\n'.format(wen_name, wen_name))
 choose = input('')
 if choose == '1':
     print('您来自 {}'.format(geo_city))
     print('\033[0;32;40m目前\033[0m', ageo_city, '的天气为', weather, '，温度', temperature, '℃，', winddirection,
           windpower, '级风，空气湿度', humidity, '，此数据最后更新于', reporttime, '\n')
-    print('查询完成')
-    print("按下任意键以退出程序")
+    print('已完成")
     input()
 elif choose == '2':
     print('请输入此城市的Adcode码（Adcode）：')
@@ -67,24 +61,20 @@ elif choose == '2':
     print("按下任意键以退出程序")
     input()
 elif choose == '3':
-    quit()
+    print("按下任意键以退出程序")
+    input()
 elif choose == '4':
     print(
         '\033[1mWenWeather  V0.9.4     CloudVersion: {}.{}\033[0m\nBy \033[0;32;40mAndy17269\033[0m\nGithub@Andy17269\nBilibili@干净T松鼠\nWenCenter Studio@2023 https://wenlei.club\n由\033[0;32;40m高德地图\033[0m提供天气数据。'.format(
             versiontime, version))
-    print("按下任意键以退出程序")
-    input()
 elif choose == '7890':
-    print('+ -- + -- + -- + -- +')
     print('____________________________________________________')
     print('\nDeveloper Tools 已启动')
     print('____________________________________________________')
     print('CityWeather: ',wa)
-    print('CloudConfig: ',config_info)
+    print('COS_CloudConfig: ',config_info)
     print('Geo: ',ageo)
-    print("按下任意键以退出程序")
+    print("按下回车以退出程序")
     input()
 else:
-    print('请选择正确的功能选项，再次进入后重试。')
-    print("按下任意键以退出程序")
-    input()
+    print('请选择正确的功能选项，再次进入后重试')
